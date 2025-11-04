f5509e17cdab62c505dbb1970c67c28aec6f5166
plturrell <98904375+plturrell@users.noreply.github.com>
2025-11-04 12:29:06 +0800

    chore: remove temporary fix file

--- services/graph/pkg/workflows/orchestration_processor_fix.go
+++
@@ -1,8 +0,0 @@
-// This is a temporary file to help with the replacement
-// The QueryKnowledgeGraphForChainNode function needs to be updated
-
-package workflows
-
-// The actual implementation should replace lines 190-244 in orchestration_processor.go
-// with the Neo4j query implementation
-
