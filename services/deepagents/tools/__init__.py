5bebfb4a0f396be9f662c73ee9805a0108068bdf
plturrell <98904375+plturrell@users.noreply.github.com>
2025-11-05 13:14:20 +0800

    Update tools __init__.py to export GPU tools

--- services/deepagents/tools/__init__.py
+++ services/deepagents/tools/__init__.py
@@ -3,10 +3,15 @@
 from .knowledge_graph_tool import query_knowledge_graph
 from .agentflow_tool import run_agentflow_flow
 from .orchestration_tool import run_orchestration_chain
+from .gpu_tool import allocate_gpu, release_gpu, query_gpu_status, analyze_workload
 
 __all__ = [
     "query_knowledge_graph",
     "run_agentflow_flow",
     "run_orchestration_chain",
+    "allocate_gpu",
+    "release_gpu",
+    "query_gpu_status",
+    "analyze_workload",
 ]
 
